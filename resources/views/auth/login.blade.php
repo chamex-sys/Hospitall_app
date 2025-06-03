4814bfc0e269d229709130d01c70b9ed0b8d238b
chamex-sys <charifchaima272@gmail.com>
2025-06-03 14:12:52 +0000

    Update login.blade.php

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,8 +1,9 @@
+[media pointer="file-service://file-2YaNvEoh57TGFf7mrJyyff"]
+
 <x-guest-layout>
     <x-authentication-card>
-        {{-- Logo personnalisé --}}
         <x-slot name="logo">
-            <img src="{{ asset('images/logo.png') }}" alt="Logo" class="w-20 h-20 mx-auto">
+            <x-authentication-card-logo />
         </x-slot>
 
         <x-validation-errors class="mb-4" />
@@ -18,12 +19,12 @@
 
             <div>
                 <x-label for="email" value="{{ __('Email') }}" />
-                <x-input id="email" class="block mt-1 w-full border-gray-300 focus:border-green-400 focus:ring focus:ring-green-200 focus:ring-opacity-50" type="email" name="email" :value="old('email')" required autofocus autocomplete="username" />
+                <x-input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email')" required autofocus autocomplete="username" />
             </div>
 
             <div class="mt-4">
                 <x-label for="password" value="{{ __('Password') }}" />
-                <x-input id="password" class="block mt-1 w-full border-gray-300 focus:border-green-400 focus:ring focus:ring-green-200 focus:ring-opacity-50" type="password" name="password" required autocomplete="current-password" />
+                <x-input id="password" class="block mt-1 w-full" type="password" name="password" required autocomplete="current-password" />
             </div>
 
             <div class="block mt-4">
@@ -35,16 +36,15 @@
 
             <div class="flex items-center justify-end mt-4">
                 @if (Route::has('password.request'))
-                    <a class="underline text-sm text-gray-600 hover:text-green-500 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-green-400" href="{{ route('password.request') }}">
+                    <a class="underline text-sm text-gray-600 hover:text-gray-900 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500" href="{{ route('password.request') }}">
                         {{ __('Forgot your password?') }}
                     </a>
                 @endif
 
-                {{-- Bouton vert personnalisé --}}
-                <x-button class="ms-4 bg-[#00D8B5] hover:bg-[#00bfa1] text-white">
+                <x-button class="ms-4">
                     {{ __('Log in') }}
                 </x-button>
             </div>
         </form>
     </x-authentication-card>
-</x-guest-layout>
+</x-guest-layout>    
