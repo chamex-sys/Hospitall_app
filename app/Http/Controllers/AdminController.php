f05e4dabb593bc174942355220b4e427b17b4fe5
chamex-sys <charifchaima272@gmail.com>
2025-06-03 10:58:01 +0000

    Update AdminController.php

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -59,5 +59,21 @@ public function updatedoctor($id){
 return view('admin.update_doctor',compact('data'));
     
 }
+    public function editdoctor(Request $request , $id){
+$doctor = doctor::find($id);
+$doctor->name=$request->name ;
+$doctor->phone=$request->phone ;
+$doctor->speciality=$request->speciality ;
+$doctor->room=$request->room ;
+$image=$request->file ;
+        if($image)
+        {
+$imagename=time().'.'.$image->getOriginalClientExtension();
+$request->file->move('doctorimage',$imagename);
+$doctor->image=$imagename ; }
+    $doctor->save();
+        return redirect->back();
+        
+    }
 
 }
