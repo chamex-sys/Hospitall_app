1bf187b757eff807e109ed53fbe0624fa4ec0c37
chamex-sys <charifchaima272@gmail.com>
2025-06-04 21:25:58 +0000

    Update Doctor.php

--- app/Models/Doctor.php
+++ app/Models/Doctor.php
@@ -6,5 +6,9 @@ use Illuminate\Database\Eloquent\Model;
 
 class Doctor extends Model
 {
-    //
+    public function appointments()
+{
+    return $this->hasMany(Appointment::class, 'doctor', 'name');
+}
+
 }
