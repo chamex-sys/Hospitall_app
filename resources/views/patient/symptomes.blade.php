520a8f1e62470760f074256db307f8ddef9e886b
chamex-sys <charifchaima272@gmail.com>
2025-06-04 14:45:19 +0000

    Update symptomes.blade.php

--- resources/views/patient/symptomes.blade.php
+++ resources/views/patient/symptomes.blade.php
@@ -1,1 +1,15 @@
+@extends('layouts.app')
 
+@section('content')
+<div class="container">
+    <h2>Décrivez vos symptômes</h2>
+    <form action="{{ route('suggest.doctor') }}" method="POST">
+        @csrf
+        <div class="form-group">
+            <label for="symptoms">Symptômes :</label>
+            <textarea name="symptoms" class="form-control" required></textarea>
+        </div>
+        <button type="submit" class="btn btn-primary mt-2">Suggérer un docteur</button>
+    </form>
+</div>
+@endsection
