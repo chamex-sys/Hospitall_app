d511b60d20317c45636ab175ae1bd6249dfdee0a
chamex-sys <charifchaima272@gmail.com>
2025-06-04 23:09:24 +0000

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,8 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\HomeController;
 use App\Http\Controllers\AdminController;
+use App\Http\Controllers\DoctorController;
+
 
  
 Route::get('/', [HomeController::class, 'index']);
