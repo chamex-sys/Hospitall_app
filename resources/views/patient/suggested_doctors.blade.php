69bb7b1c01f312900e56c2f6e0d7d4f921e14e14
chamex-sys <charifchaima272@gmail.com>
2025-06-04 14:51:04 +0000

    Update suggested_doctors.blade.php

--- resources/views/patient/suggested_doctors.blade.php
+++ resources/views/patient/suggested_doctors.blade.php
@@ -1,1 +1,25 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+    <h2>suggested doctors</h2>
+
+    @if($doctors->isEmpty())
+        <div class="alert alert-warning">
+            No doctor is suggested for these symptoms .
+        </div>
+    @else
+        <p><strong>Detected Speciality :</strong> {{ ucfirst($speciality) }}</p>
+        <ul class="list-group">
+            @foreach($doctors as $doctor)
+                <li class="list-group-item">
+                    <strong>{{ $doctor->name }}</strong> - Chambre {{ $doctor->room }}
+                    <br>
+                    Spécialité : {{ $doctor->speciality }}
+                </li>
+            @endforeach
+        </ul>
+    @endif
+</div>
+@endsection
 
