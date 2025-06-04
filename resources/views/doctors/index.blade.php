4e3ff22b8e53b0a0a65d67b00f76d3239d72f86c
chamex-sys <charifchaima272@gmail.com>
2025-06-04 22:42:34 +0000

    Update index.blade.php

--- resources/views/doctors/index.blade.php
+++ resources/views/doctors/index.blade.php
@@ -1,1 +1,14 @@
+@extends('layouts.app')
+
+@section('content')
+<h1>Liste des Docteurs</h1>
+@foreach ($doctors as $doctor)
+    <div style="border:1px solid #ccc; margin:10px; padding:10px;">
+        <h3>{{ $doctor->name }}</h3>
+        <p>Spécialité: {{ $doctor->speciality }}</p>
+        <p>Chambre: {{ $doctor->room }}</p>
+        <a href="{{ route('doctors.calendar', $doctor->id) }}">📅 Voir le calendrier</a>
+    </div>
+@endforeach
+@endsection
 
