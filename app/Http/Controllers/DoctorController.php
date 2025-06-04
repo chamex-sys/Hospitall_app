fc8c55b383b369f0eb8bb0c7285efc26b3dccf8b
chamex-sys <charifchaima272@gmail.com>
2025-06-04 22:40:55 +0000

    Update DoctorController.php

--- app/Http/Controllers/DoctorController.php
+++ app/Http/Controllers/DoctorController.php
@@ -3,8 +3,23 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Doctor;
+use App\Models\Appointment;
 
 class DoctorController extends Controller
 {
-    //
+    
+    public function index()
+    {
+        $doctors = Doctor::all();
+        return view('doctors.index', compact('doctors'));
+    }
+
+    public function calendar($id)
+    {
+        $doctor = Doctor::findOrFail($id);
+        $appointments = Appointment::where('doctor', $doctor->name)->get();
+        
+        return view('doctors.calendar', compact('doctor', 'appointments'));
+    }
 }
