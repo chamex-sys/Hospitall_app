0827b634d17c97e5fa4e6ffc297d86add80b2068
chamex-sys <charifchaima272@gmail.com>
2025-06-06 06:48:09 +0000

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -4,6 +4,8 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\HomeController;
 use App\Http\Controllers\AdminController;
 use App\Http\Controllers\DoctorController;
+use App\Http\Controllers\SymptomController;
+
 
 
  
@@ -33,6 +35,9 @@ Route::get('/doctors', [DoctorController::class, 'index'])->name('doctors.index'
 Route::get('/doctors/{id}/calendar', [DoctorController::class, 'calendar'])->name('doctors.calendar');
 Route::get('/symptoms', [App\Http\Controllers\SymptomController::class, 'form']);
 Route::post('/symptoms', [App\Http\Controllers\SymptomController::class, 'analyze']);
+
+Route::post('/suggest-doctor', [SymptomController::class, 'suggest'])->name('suggest.doctor');
+
 
 
 
